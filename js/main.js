0f737eb9ef9ec468aa24294388eb51cd29f9515b
mehediemon007 <mehediemon7@gmail.com>
2022-04-04 11:39:42 +0600

    slide-change

--- js/main.js
+++ js/main.js
@@ -58,89 +58,176 @@
   }
   
     // Banner
-  
-    // var interleaveOffset = 0.5;
-  
-    var mySwiper = new Swiper ('.ds-banner', {
-        // Optional parameters
-        direction: 'horizontal',
+
+    // Params
+    const mpSlider = document.querySelector('.multi-px-slider');
+    let interleaveOffset = 0.5;
+
+    // init small slider
+    const smallSlider = new Swiper('.sm-slider', {
+        touchRatio: 0, // disable swipe
         loop: true,
-        speed: 1200,
-    
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-          type: 'fraction',
-          clickable: true,
+        slidesPerView: 'auto',
+        preloadImages: false,
+        lazy: {
+            loadPrevNext: true,
+            loadPrevNextAmount: 2,
         },
-    
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-  
-        autoplay: {
-            delay: 2000,
-        },
-  
-        // on: {
-        //   progress: function() {
-        //     var swiper = this;
-        //     for (var i = 0; i < swiper.slides.length; i++) {
-        //       var slideProgress = swiper.slides[i].progress;
-        //       var innerOffset = swiper.width * interleaveOffset;
-        //       var innerTranslate = slideProgress * innerOffset;
-        //       swiper.slides[i].querySelector(".single-slide").style.transform =
-        //         "translate3d(" + innerTranslate + "px, 0, 0)";
-        //     }      
-        //   },
-        //   touchStart: function() {
-        //     var swiper = this;
-        //     for (var i = 0; i < swiper.slides.length; i++) {
-        //       swiper.slides[i].style.transition = "";
-        //     }
-        //   },
-        //   setTransition: function(speed) {
-        //     var swiper = this;
-        //     for (var i = 0; i < swiper.slides.length; i++) {
-        //       swiper.slides[i].style.transition = speed + "ms";
-        //       swiper.slides[i].querySelector(".single-slide").style.transition =
-        //         speed + "ms";
-        //     }
-        //   }
-        // }
-        // on: {
-        //   slideChangeTransitionStart: function () {
-        //       // Slide captions
-        //       var swiper = this;
-        //       setTimeout(function () {
-        //         var currentTitle = $(swiper.slides[swiper.activeIndex]).attr("data-title");
-        //         var currentSubtitle = $(swiper.slides[swiper.activeIndex]).attr("data-subtitle");
-        //       }, 500);
-        //       gsap.to($(".current-title"), 0.4, {autoAlpha: 0, y: -40, ease: Power1.easeIn});
-        //       gsap.to($(".current-subtitle"), 0.4, {autoAlpha: 0, y: -40, delay: 0.15, ease: Power1.easeIn});
-        //   },
-        //   slideChangeTransitionEnd: function () {
-        //           // Slide captions
-        //       var swiper = this;
-        //       var currentTitle = $(swiper.slides[swiper.activeIndex]).attr("data-title");
-        //       var currentSubtitle = $(swiper.slides[swiper.activeIndex]).attr("data-subtitle");
-        //       $(".slide-captions").html(function() {
-        //         return "<h2 class='current-title'>" + currentTitle + "</h2>" + "<h3 class='current-subtitle'>" + currentSubtitle + "</h3>";
-        //       });
-        //       gsap.from($(".current-title"), 0.4, {autoAlpha: 0, y: 40, ease: Power1.easeOut});
-        //       gsap.from($(".current-subtitle"), 0.4, {autoAlpha: 0, y: 40, delay: 0.15, ease: Power1.easeOut});
-        //   }
-        // }
+        watchSlidesProgress: true,
+        watchSlidesVisibility: true,
+        on: {
+            init: function(){
+                let swiper = this;
+            },
+            lazyImageReady: function(){
+                let swiper = this;
+
+                setTimeout(function() {
+                    swiper.update();
+                }, 500);
+            },
+            progress: function(){
+                let swiper = this;
+                for (let i = 0; i < swiper.slides.length; i++) {
+                    let slideProgress = swiper.slides[i].progress,
+                        innerOffset = swiper.width * interleaveOffset,
+                        innerTranslate = slideProgress * innerOffset;
+                    swiper.slides[i].querySelector(".img-sm").style.transform = "translateX(" + innerTranslate + "px)";
+                }
+            },
+            touchStart: function() {
+                let swiper = this;
+                for (let i = 0; i < swiper.slides.length; i++) {
+                    swiper.slides[i].style.transition = "";
+                }
+            },
+            setTransition: function(speed) {
+                let swiper = this;
+                for (let i = 0; i < swiper.slides.length; i++) {
+                    swiper.slides[i].style.transition = speed + "ms";
+                    swiper.slides[i].querySelector(".img-sm").style.transition = speed + "ms";
+                }
+            }
+        }
     });
-    
-    // Slide captions
-    var currentTitle = $(mySwiper.slides[mySwiper.activeIndex]).attr("data-title");
-    var currentSubtitle = $(mySwiper.slides[mySwiper.activeIndex]).attr("data-subtitle");
-    $(".slide-captions").html(function() {
-      return "<h2 class='current-title'>" + currentTitle + "</h2>" + "<h3 class='current-subtitle'>" + currentSubtitle + "</h3>";
+
+
+    // init large slider
+    const largeSlider = new Swiper('.lg-slider', {
+        parallax: true,
+        loop: true,
+        speed: 2000,
+        slidesPerView: 'auto',
+        preloadImages: false,
+        lazy: {
+            loadPrevNext: true,
+            loadPrevNextAmount: 2,
+        },
+        watchSlidesProgress: true,
+        watchSlidesVisibility: true,
+        touchEventsTarget: 'wrapper',
+        controller: {
+            control: smallSlider
+        },
+        on: {
+            init: function(){
+                let swiper = this;
+            },
+            lazyImageReady: function(){
+                let swiper = this;
+
+                setTimeout(function() {
+                    swiper.update();
+                    mpSlider.classList.remove('loading');
+                }, 500);
+            },
+            progress: function(){
+                let swiper = this;
+                for (let i = 0; i < swiper.slides.length; i++) {
+                    let slideProgress = swiper.slides[i].progress,
+                        innerOffset = swiper.width * interleaveOffset,
+                        innerTranslate = slideProgress * innerOffset;
+                    swiper.slides[i].querySelector(".img-lg").style.transform = "translateX(" + innerTranslate + "px)";
+                }
+            },
+            touchStart: function() {
+                let swiper = this;
+                for (let i = 0; i < swiper.slides.length; i++) {
+                    swiper.slides[i].style.transition = "";
+                }
+            },
+            setTransition: function(speed) {
+                let swiper = this;
+                for (let i = 0; i < swiper.slides.length; i++) {
+                    swiper.slides[i].style.transition = speed + "ms";
+                    swiper.slides[i].querySelector(".img-lg").style.transition = speed + "ms";
+                }
+            }
+        }
     });
+
+
+    // Set up animations
+    let slideDelay = 2000;
+    let $mpsArrow = $('.mps-arrow');
+
+    function runAnimation() {
+        mpSlider.classList.add('is-animating');
+    }
+
+    function endAnimation() {
+        mpSlider.classList.remove('is-animating');
+    }
+
+
+    // custom arrows
+    $mpsArrow.each((i, el) => {
+        const _this = $(el);
+
+      _this.on('click', function() {
+
+              // disable arrows
+              $mpsArrow.prop('disabled', true);
+              // run animation
+              runAnimation();
+
+              // go to prev/next slide
+              if (_this.hasClass('mps-prev')) {
+                  setTimeout(() => {
+                    largeSlider.slidePrev();
+                  }, slideDelay)
+              } else if (_this.hasClass('mps-next')) {
+                  setTimeout(() => {
+                    largeSlider.slideNext();
+                  }, slideDelay)
+              }
+
+              // detect animation end
+              const curtain = document.querySelector('.curtain');
+              curtain.addEventListener('animationend', () => {
+                  // re-enable arrows
+                $mpsArrow.prop('disabled', false);
+                // end animation
+                endAnimation();
+              });
+
+      })
+
+    })
+
+
+    // TO DO: check if slider is in viewport?
+    let mpsInViewport = true;
+
+    if (mpsInViewport) {
+        $(document).off('keyup').on('keyup', function(e) {
+              if (e.keyCode == '37') {
+                  $('.mps-prev').trigger('click');
+              } else if (e.keyCode == '39') {
+                  $('.mps-next').trigger('click');
+              }
+      });
+    }
   
   })()
   
