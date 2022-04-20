ca9ec1ca87b3ad05fef00f005ba60bc6b18cf34a
mehediemon007 <mehediemon7@gmail.com>
2022-04-20 17:08:49 +0600

    totop

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 ;(function($){
+
+  var totop = $('#toTop'),
+      win = $(window);
+
+  // scrollTop init
+  win.on('scroll', function () {
+    if (win.scrollTop() > 150) {
+        totop.fadeIn();
+    } else {
+        totop.fadeOut();
+    }
+  });
+
+  // totop scroller
+  totop.on('click', function () {
+    $("html,body").animate({
+        scrollTop: 0
+    }, 500)
+});
+
   
     // Counter
 
